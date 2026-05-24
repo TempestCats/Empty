4e0726152e3bb95bd18df9e3c6254c38d59326ca
TempestCats <1823118258@qq.com>
2026-05-24 20:53:41 +0800

    更新了Astro框架

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -44,7 +44,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "RSS",
 			icon: "fa7-solid:rss",
-			url: "https://blog.216725.xyz/rss.xml",
+			url: "/rss/",
 			showName: false,
 		},
 	],
